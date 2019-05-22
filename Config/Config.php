5d3930db6691fb1e85c1f97e66fcf973d9fcda24
brian <brian@gmail.com>
2019-05-22 09:06:14 +0200

    Anidiendo puerto, charset, y configuraciones para esta compu hp

--- Config/Config.php
+++ Config/Config.php
@@ -6,8 +6,10 @@
         const tipoDB = 'mysql';
         const servidor = 'localhost';
         const user = 'root';
-        const password = '';
+        const password = 'brian123';
         const db = 'acuatic';
+        const port = '3306';
+        const charset = 'utf8mb4';
     }
 
 ?>
