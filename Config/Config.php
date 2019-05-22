91c5b19818eef5cbc21d39fd1bddb9e03cfcb017
playerblade <evotar156@gmail.com>
2019-05-22 09:50:02 -0400

    Comente En la carpeta de config

--- Config/Config.php
+++ Config/Config.php
@@ -2,14 +2,14 @@
 
     class Config 
     {
-        const webRoot = '/ProjectMVC';
-        const tipoDB = 'mysql';
-        const servidor = 'localhost';
-        const user = 'root';
-        const password = 'brian123';
-        const db = 'acuatic';
-        const port = '3306';
-        const charset = 'utf8mb4';
+        // const webRoot = '/ProjectMVC';
+        // const tipoDB = 'mysql';
+        // const servidor = 'localhost';
+        // const user = 'root';
+        // const password = '';
+        // const db = 'acuatic';
+        // const port = '3306';
+        // const charset = 'utf8mb4';
     }
 
 ?>
