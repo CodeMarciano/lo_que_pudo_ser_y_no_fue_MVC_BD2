f70a4d943f539ad09d2898dc69fb11ff8f8c35bf
gato <corneliosganar100@gmail.com>
2019-05-29 00:47:31 -0400

    Anidiendo login materialize

--- Login/Views/Form.php
+++ Login/Views/Form.php
@@ -1,5 +1,19 @@
-<?php require_once './resources/indexComponent/header.php'?>
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" href="<?php echo Config::webRoot?>/resources/materialize/css/materialize-icons.css">
+    <link rel="stylesheet" href="<?php echo Config::webRoot?>/resources/materialize/css/materialize.min.css">
+</head>
+<body>
 
+<div class="section container" style="width: 700px;">
+    <div class="row">
+        <div class="card-panel">
             <h3>Login</h3>
 
             <form action="<?= Config::webRoot ?>/login/verify" method="POST">
@@ -16,4 +30,10 @@
                 <button type="submit" class="btn btn-info">Aceptar</button>
 
             </form>
-<?php require_once './resources/indexComponent/footer.php'?>
+
+        </div>
+    </div>
+</div>
+<script src="<?php echo Config::webRoot?>/resources/materialize/js/materialize.min.js"></script>
+</body>
+</html>
